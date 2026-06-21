3f9071955136805115540cbd06c689b28b7ea96b
MalikHanzla <155191324+MalikHanzla@users.noreply.github.com>
2026-06-21 13:29:26 +0500

    Update bot settings with new author and pack name

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Kenny Bot',
-  author: 'Wambugu Kinyua',
-  botName: "Kinyua Bot",
-  botOwner: 'Wambugu Kinyua', // Your name
-  ownerNumber: '2547xxxxxx', //Set your number here without + symbol, just add country code & number without any space
+  packname: 'Absent',
+  author: 'Malik',
+  botName: "Absent",
+  botOwner: 'Malik', // Your name
+  ownerNumber: '923391021526', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
